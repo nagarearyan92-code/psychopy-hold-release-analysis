38704514b380c1bf7897cddac0ee0c7ada373490
Aryan Nagare <nagarearyan92@gmail.com>
2026-07-04 04:43:34 +0100

    Add files via upload

--- hold_release_demo_v5_skip_release_signal_lastrun.py
+++ hold_release_demo_v5_skip_release_signal_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2026.1.3),
-    on July 04, 2026, at 01:51
+    on July 04, 2026, at 04:42
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
